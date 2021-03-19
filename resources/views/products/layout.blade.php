54bae47559e2b71f634a179024d44343dea8c720
Piotr <piotrmizerka@gmail.com>
2021-03-19 22:54:46 +0100

    Bootstrap source corrected to display layout properly

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Product Store</title>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
     </head>
     <body>
         <h1>Product Store</h1>
