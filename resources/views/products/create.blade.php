7c3b61489b60ea8e8289c9680a576f0d316e78e7
Piotr <piotrmizerka@gmail.com>
2021-03-19 21:21:39 +0100

    Create and layout blades

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,7 @@
+@extends('products.layout')
+
+@section('content')
+
+<h1>Create Product ...</h1>
+
+@endsection
---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Product Store</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Product Store</h1>
+        <div class="container">
+            <br>
+            @yield('content')
+        </div>
+    </body>
+</html>
