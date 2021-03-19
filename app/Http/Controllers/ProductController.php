692fae37747c14652b351cb5e1a93ce07f78246d
Piotr <piotrmizerka@gmail.com>
2021-03-19 23:25:43 +0100

    Product list controller function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::latest()->paginate(5);
+        return view('products.index',compact('products'))->with(request()->input('page'));
     }
 
     /**
