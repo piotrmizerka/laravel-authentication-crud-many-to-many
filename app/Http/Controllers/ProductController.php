2a333bdabdcc86c42d4035a47fd80374f831f181
Piotr <piotrmizerka@gmail.com>
2021-03-19 23:44:03 +0100

    Displaying product info

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -58,7 +58,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return view('products.show',compact('product'));
     }
 
     /**
